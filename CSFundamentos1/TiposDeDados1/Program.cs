3c2b584428cd1238ff737853ba05830417d16544
jhonatacamargol <jhonatapires32@hotmail.com>
2023-05-18 15:51:41 -0300

    commit um

---
+++ CSFundamentos1/TiposDeDados1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Declarando variáveis numéricas");
+Console.WriteLine();
+//declarando variável
+byte valor1 = 255;
+sbyte valor2 = -127;
+int valor3 = -2147482647;
+uint valor4 = 2147482647;
+long valor5 = -21474826489;
+
+Console.WriteLine(valor1);
+Console.WriteLine(valor2);
+Console.WriteLine(valor3);
+Console.WriteLine(valor4);
+Console.WriteLine(valor5);
+
+Console.ReadLine();
