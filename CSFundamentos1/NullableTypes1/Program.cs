7784ad41a5b8b7e6642227ec7796057c40d29a1f
jhonatacamargol <jhonatapires32@hotmail.com>
2023-05-24 13:50:29 -0300

    novo commit

---
+++ CSFundamentos1/NullableTypes1/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.WriteLine("## Nullable Types ##\n");
+
+Nullable<int> i = null;
+Nullable<double> d = null;
+Nullable<bool> b = null;
+
+int? i1 = null;
+double? d1 = null;
+bool? b1 = null;
+
+Console.WriteLine(i);
+Console.WriteLine(d);
+Console.WriteLine(b);
+
+Console.WriteLine(i1);
+Console.WriteLine(d1);
+Console.WriteLine(b1);
+
+int? a = 1;
+int b2 = a ?? 0;
+
+Console.WriteLine(b2);
+
+int? x = 4;
+int? y = 3;
+int? z = x * y;
+
+Console.WriteLine(z);
+
+int? m = 100;
+if (m.HasValue)
+{
+    Console.WriteLine($"b = {m.Value}");
+}
+else { Console.WriteLine("m não possui um valor (null)");
+}
+
+int? n = null;
+if (n.HasValue)
+{
+    Console.WriteLine($"n = {n.Value}");
+}
+else
+{
+    Console.WriteLine("n não possui um valor (null)");
+}
+
+
+Console.ReadKey();
