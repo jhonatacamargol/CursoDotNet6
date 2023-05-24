d04cbd267ecdc9dad656ccbd730e8b6fd59ccebe
jhonatacamargol <jhonatapires32@hotmail.com>
2023-05-24 12:37:55 -0300

    novo commit

---
+++ CSFundamentos1/TipoDataTime1/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("## Struct DataTime ##\n");
+
+DateTime hoje = DateTime.Now;
+Console.WriteLine($"hoje : {hoje}\n");
+
+//extrair informações da data atual
+Console.WriteLine(hoje.Year);
+Console.WriteLine(hoje.Month);
+Console.WriteLine(hoje.Day);
+Console.WriteLine(hoje.Hour);
+Console.WriteLine(hoje.Minute);
+Console.WriteLine(hoje.Second);
+Console.WriteLine(hoje.Millisecond);
+
+//adcionando valores
+Console.WriteLine(hoje.AddDays(30));
+Console.WriteLine(hoje.AddMonths(1));
+Console.WriteLine(hoje.AddHours(2));
+Console.WriteLine(hoje.AddYears(5));
+
+//obter o dia da semana e do ano
+Console.WriteLine(hoje.DayOfWeek);
+Console.WriteLine(hoje.DayOfYear);
+
+//data formato longo e curto
+Console.WriteLine(hoje.ToLongDateString());
+Console.WriteLine(hoje.ToShortDateString());
+
+Console.WriteLine(hoje.ToLongTimeString());
+Console.WriteLine(hoje.ToShortTimeString());
+
+//criar uma data específica usa formato: aaaa,mm,dd
+DateTime DataHoje = new DateTime(2023, 10, 01);
+Console.WriteLine(DataHoje);
+
+//Definir horas
+DateTime DataHoraHoje = new DateTime(2023, 10, 01, 21, 10, 30);
+Console.WriteLine(DataHoraHoje);
+
+Console.ReadKey();
---
+++ CSFundamentos1/TiposDeDados4/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Definindo variáveis do tipo string, object e dynamic");
+Console.WriteLine();
+
+string nome = "Curso C#";
+String titulo = "Curso C# Essencial";
+
+Console.WriteLine(nome);
+Console.WriteLine(titulo);
+
+string valor = "Isso é uma string";
+
+valor = "Isso é uma string alterada";
+
+valor = "Teste";
+
+//StringBuilder
+
+object nota = 10;
+object valor1 = 8.55;
+object nome1 = "Maria";
+object ativa = true;
+object letra = 'A';
+
+Console.WriteLine(nota);
+Console.WriteLine(valor1);
+Console.WriteLine(nome1);
+Console.WriteLine(ativa);
+Console.WriteLine(letra);
+
+Console.ReadLine();
