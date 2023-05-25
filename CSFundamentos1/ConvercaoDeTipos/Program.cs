4f776e17db6f2f770a8cc6ee2de4766d392cbae6
jhonatacamargol <jhonatapires32@hotmail.com>
2023-05-25 19:09:22 -0300

    novo commit

---
+++ CSFundamentos1/ConvercaoDeTipos/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("## Converção de Tipos ##\n");
+
+int valorInt = 10;
+double valorDouble = 5.35;
+bool valorBool = true;
+
+Console.WriteLine(Convert.ToString(valorInt));
+Console.WriteLine(Convert.ToDouble(valorInt));
+Console.WriteLine(Convert.ToString(valorBool));
+Console.WriteLine(Convert.ToInt32(valorDouble));
+
+int varInt = 100000;
+Console.WriteLine(Convert.ToString(varInt));
+
+Console.ReadKey();
---
+++ CSFundamentos1/EntradaDeDados/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("\n## Entrada de dados ##");
+
+Console.WriteLine("\nInforme seu nome:");
+
+string nome = Console.ReadLine();
+
+Console.WriteLine("\nO seu nome é " + nome);
+
+Console.WriteLine("\nInforme sua idade:");
+
+int idade = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Sua idade é {idade}\n");
+Console.WriteLine($"Seu nome é {nome} e sua idade é {idade} anos.");
+
+
+Console.ReadKey();
