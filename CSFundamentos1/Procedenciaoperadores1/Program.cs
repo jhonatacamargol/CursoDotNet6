f9aa5e493f1830dd22bfe056b1b491e0b64b69ad
jhonatacamargol <jhonatapires32@hotmail.com>
2023-05-29 13:25:31 -0300

    novo commit

---
+++ CSFundamentos1/Procedenciaoperadores1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("## Procedência de operadores ##\n");
+
+int x = 10 - 2 * 3;
+Console.WriteLine(x);
+
+int y = (10 - 2) * 3;
+Console.WriteLine(y);
+
+bool b = !(9 != 8) && 5 >= 7 || 8 >= 6;
+Console.WriteLine(b);
+
+bool c = !(9 != 8) && (5 >= 7 || 8 >= 6);
+Console.WriteLine(c);
+
+int m = 5, n = 6, o = 4;
+int r = --m * n - ++o;
+Console.WriteLine(r);
+
+Console.ReadKey();
