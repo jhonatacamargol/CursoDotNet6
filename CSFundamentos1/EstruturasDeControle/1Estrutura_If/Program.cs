b7158a61aa9fc1c1c1b965d5c99c28b99a5a1dc8
jhonatacamargol <jhonatapires32@hotmail.com>
2023-06-30 11:28:41 -0300

    novo commit

---
+++ CSFundamentos1/EstruturasDeControle/1Estrutura_If/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("## Instrução if ##\n");
+
+Console.WriteLine("Cliente especial (true/false)");
+var resposta = Convert.ToBoolean(Console.ReadLine());
+
+if (resposta)
+{
+    Console.WriteLine("DESCONTO DE 10%");
+}
+Console.WriteLine("Fim do processamento");
+
+
+Console.WriteLine("Informe o valor de x\n");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Informe o valor de y\n");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > y)
+{
+    Console.WriteLine("x é maior que y");
+} if (x < y)
+{
+    Console.WriteLine("x é menor que y");
+} if (x ==  y)
+{
+    Console.WriteLine("x é igual a y");
+}
+
+Console.ReadKey();
