2e332aaff71a6227d148be4b0f46793a96516a94
jhonatacamargol <jhonatapires32@hotmail.com>
2023-07-22 17:37:32 -0300

    novo commit

---
+++ CSFundamentos1/Estrutura_if_else_if/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Informe a nota do aluno:\t");
+var nota = Convert.ToDouble(Console.ReadLine());
+
+if(nota < 5)
+{
+    Console.WriteLine("ALUNO REPROVADO.");
+}
+else if(nota >=5 && nota <6)
+{
+    Console.WriteLine("ALUNO EM RECUPERAÇÃO.");
+}
+else if (nota >=6 && nota < 9)
+{
+    Console.WriteLine("ALUNO APROVADO.");
+}
+else if (nota >= 9)
+{
+    Console.WriteLine("ALUNO APROVADO COM DISTINÇÃO.");
+}
+Console.WriteLine("FIM DO PROCESSAMENTO.");
+
+Console.ReadKey();
